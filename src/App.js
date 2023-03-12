4ec5c5ef8d5b962cd6f335d55f1f59b9985f9d7c
Jean Wilker <jeanwilkerss@gmail.com>
2023-03-11 23:56:16 -0300

    Cria rota protegina para página do usuário.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Home from './pages/Home/Home';
 import Login from './components/Login/Login';
+import User from './pages/User/User';
 
 import './App.css';
+import ProtectedRoute from './components/Header/ProtectedRoute/ProtectedRoute';
 
 function App() {
     return (
@@ -16,7 +18,15 @@ function App() {
                     <Header />
                     <Routes>
                         <Route path="/" element={<Home />} />
-                        <Route path="/login/*" element={<Login />} />
+                        <Route path="login/*" element={<Login />} />
+                        <Route
+                            path="conta/*"
+                            element={
+                                <ProtectedRoute>
+                                    <User />
+                                </ProtectedRoute>
+                            }
+                        />
                     </Routes>
                     <Footer />
                 </UserStorage>
