228d0480ccf15628f58d760d78d5b72c1b8c0e4d
Jean Wilker <jeanwilkerss@gmail.com>
2023-04-19 23:10:35 -0300

    Cria scroll infinito para paginação do feed.

--- src/components/Feed/FeedPhotos/FeedPhotos.jsx
+++ src/components/Feed/FeedPhotos/FeedPhotos.jsx
@@ -8,16 +8,22 @@ import { PHOTOS_GET } from '../../../services/api';
 
 import styles from './FeedPhotos.module.css';
 
-const FeedPhotos = ({ user, setModalPhoto }) => {
+const FeedPhotos = ({ page, setInfinite, user, setModalPhoto }) => {
     const { data, loading, error, request } = useFetch();
 
     useEffect(() => {
         async function fetchPhotos() {
-            const { url, options } = PHOTOS_GET({ page: 1, total: 6, user });
+            const total = 6;
+
+            const { url, options } = PHOTOS_GET({ page, total, user });
             const { response, json } = await request(url, options);
+
+            if (response && response.ok && json.length < total) {
+                setInfinite(false);
+            }
         }
         fetchPhotos();
-    }, [request, user]);
+    }, [request, page, setInfinite, user]);
 
     if (error) return <Error error={error} />;
     if (loading) return <Loading />;
