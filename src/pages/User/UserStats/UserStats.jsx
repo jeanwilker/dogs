f3f9192bd90e4710977e51554b32c0114bea85d4
Jean Wilker <jeanwilkerss@gmail.com>
2023-04-26 00:55:44 -0300

    Cria página de gráficos de acesso com a biblioteca victory.

--- src/pages/User/UserStats/UserStats.jsx
+++ src/pages/User/UserStats/UserStats.jsx
@@ -1,11 +1,35 @@
+import { useEffect, lazy, Suspense } from 'react';
+import useFetch from '../../../Hooks/useFetch';
+
 import Head from '../../../components/Helper/Head/Head';
+import Loading from '../../../components/Helper/Loading/Loading';
+import Error from '../../../components/Helper/Error/Error';
+
+import { STATS_GET } from '../../../services/api';
+
+const UserStatsGraphs = lazy(() => import('./UserStatsGraphs/UserStatsGraphs'));
 
 const UserStats = () => {
-    return (
-        <div>
-            <Head title="Estatísticas" />
-        </div>
-    );
+    const { data, error, loading, request } = useFetch();
+
+    useEffect(() => {
+        async function getData() {
+            const { url, options } = STATS_GET();
+            await request(url, options);
+        }
+        getData();
+    }, [request]);
+
+    if (loading) return <Loading />;
+    if (error) return <Error error={error} />;
+    if (data)
+        return (
+            <Suspense fallback={<div></div>}>
+                <Head title="Estatísticas" />
+                <UserStatsGraphs data={data} />
+            </Suspense>
+        );
+    else return null;
 };
 
 export default UserStats;
